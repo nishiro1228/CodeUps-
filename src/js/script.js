480ff51a0ab316cf6f125fc7c7d036a74047c130
nishiro1228 <akib.1228@gmial.com>
2024-07-26 02:42:16 +0900

    2回目のコミット

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,72 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+ // ハンバーガーメニュー
+ $(function () {
+  $(".js-hamburger").click(function () {
+      $(this).toggleClass("is-active");
+      $(".js-drawer").fadeToggle();
+  });
 
+  // ドロワーナビのaタグをクリックで閉じる
+  $(".js-drawer a[href]").on("click", function () {
+      $(".js-hamburger").removeClass(".is-active");
+      $(".js-drawer").fadeOut();
+  });
+
+  // resizeイベント
+  $(window).on('resize', function() {
+      if (window.matchMedia("(min-width: 768px)").matches) {
+          $(".js-hamburger").removeClass(".is-active");
+          $(".js-drawer").fadeOut();
+      }
+  });
+  const swiper = new Swiper(".js-swiper1", {
+    slidesPerView: 'auto', // または適切な数値を指定
+    spaceBetween: 20, // または適切な間隔を指定
+    loop: true,
+    effect: "fade",
+    speed: 3000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 3000,
+    },
+  });
+});
+
+const campaign_slideLength = document.querySelectorAll('.js-campaign-swiper .swiper-slide').length
+    $(window).resize(function () {
+      campaign_arrow();
+    });
+    campaign_arrow();
+    function campaign_arrow() {
+        if (window.matchMedia('(max-width: 767px)').matches || campaign_slideLength <= 3) {
+            $('.js-campaign-arrow').hide();
+        } else {
+            $('.js-campaign-arrow').show();
+        }
+    }
+
+    // Swiper
+    var campaign_swiper = new Swiper(".js-campaign-swiper", {
+        loop: true,
+        speed: 2000,
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+        // autoplay: {
+        //     delay: 2000,
+        //     disableOnInteraction: false,
+        // },
+        breakpoints: {
+            768: {
+                slidesPerView: 3.5,
+                spaceBetween: 40
+            }
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 
 });
+
